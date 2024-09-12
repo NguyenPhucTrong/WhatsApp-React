1d79d3192b5a8e34b89f7ea8d2dfcfe09d99e1ca
NguyenPhucTrong <95624365+NguyenPhucTrong@users.noreply.github.com>
2024-09-12 20:36:38 +0700

    test

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function login() {
-  return <div>login</div>;
+  return (
+    <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6 mt-11">
+    hi
+    </div>
+  );
 }
 
 export default login;
