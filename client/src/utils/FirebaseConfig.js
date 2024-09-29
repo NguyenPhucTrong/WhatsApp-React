4e7bbe53363e69a6de185fb78dc3dda0b8b84b7d
NguyenPhucTrong <95624365+NguyenPhucTrong@users.noreply.github.com>
2024-09-29 16:54:26 +0700

    repair env

--- client/src/utils/FirebaseConfig.js
+++ client/src/utils/FirebaseConfig.js
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDhzlfoSFjxwKYy86eV_8p8I_4R3QsFzJw",
-  authDomain: "whatsapp-8d5ac.firebaseapp.com",
-  projectId: "whatsapp-8d5ac",
-  storageBucket: "whatsapp-8d5ac.appspot.com",
-  messagingSenderId: "379181913408",
-  appId: "1:379181913408:web:f92061c6ea6bd322d696a5",
-  measurementId: "G-J7014E32L3"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
